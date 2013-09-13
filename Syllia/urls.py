4e8d93436ec4bb53d0f69c9830df423a890fe3f4
MitchelCabuloy <mitchel.cabuloy@gmail.com>
2013-09-13 13:23:48 +0800

    Implemented registration

--- Syllia/urls.py
+++ Syllia/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
    # url(r'^accounts/', include('accounts.urls', namespace="accounts")),
-   url(r'^accounts/', include('authtools.urls', namespace='accounts')),
+   url(r'^accounts/', include('authtools.urls', namespace='authtools')),
    url(r'^accounts/', include('accounts.urls', namespace='accounts')),
 )
---
+++ accounts/forms.py
@@ -0,0 +1,17 @@
+from django import forms
+
+
+class RegisterForm(forms.Form):
+    email = forms.EmailField(label='Email Address')
+    name = forms.CharField()
+    password = forms.CharField(widget=forms.PasswordInput)
+    confirm_password = forms.CharField(widget=forms.PasswordInput)
+
+    def clean_confirm_password(self):
+        password = self.cleaned_data['password']
+        confirm_password = self.cleaned_data['confirm_password']
+
+        if password != confirm_password:
+            raise forms.ValidationError("The passwords do not match.")
+
+        return password
