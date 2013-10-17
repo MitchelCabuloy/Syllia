24760e17c6c394e91717d1b03747cb1bd3fa2f9f
MitchelCabuloy <mitchel.cabuloy@gmail.com>
2013-10-17 12:22:25 +0800

    Added home, about and contact pages

--- Syllia/urls.py
+++ Syllia/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import patterns, include, url
-
-from syllabus.views import DashboardView
+from django.views.generic.base import TemplateView
 
 urlpatterns = patterns('',
    # url(r'^accounts/', include('accounts.urls', namespace="accounts")),
-   url(r'^$', DashboardView.as_view(), name='index'),
+   url(r'^$', TemplateView.as_view(template_name='home/index.html'), name='index'),
+   url(r'^about/$', TemplateView.as_view(template_name='home/about.html'), name='about'),
+   url(r'^contact/$', TemplateView.as_view(template_name='home/contact.html'), name='contact'),
    url(r'^syllabus/', include('syllabus.urls', namespace='syllabus')),
    url(r'^accounts/', include('authtools.urls', namespace='authtools')),
    url(r'^accounts/', include('accounts.urls', namespace='accounts')),
