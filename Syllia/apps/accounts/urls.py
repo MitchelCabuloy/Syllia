b3167b2c73b8805d5f840a17a9deecece28af8d5
MitchelCabuloy <mitchel.cabuloy@gmail.com>
2013-10-31 21:10:57 +0800

    Did something to urls.py to try and fix heroku error

--- Syllia/apps/accounts/urls.py
+++ Syllia/apps/accounts/urls.py
@@ -1,9 +1,9 @@
 from django.conf.urls import patterns, url
 
-from Syllia.apps.accounts.views import RegisterView, change_password, update_profile
+from Syllia.apps.accounts import views
 
 urlpatterns = patterns('',
-   url(r'^register/$', RegisterView.as_view(), name='register'),
-   url(r'^update_profile/$', update_profile, name='update_profile'),
-   url(r'^change_password/$', change_password, name='change_password'),
+   url(r'^register/$', views.RegisterView.as_view(), name='register'),
+   url(r'^update_profile/$', views.update_profile, name='update_profile'),
+   url(r'^change_password/$', views.change_password, name='change_password'),
 )
--- Syllia/apps/syllabus/urls.py
+++ Syllia/apps/syllabus/urls.py
@@ -1,14 +1,12 @@
 from django.conf.urls import patterns, url
 from django.contrib.auth.decorators import login_required
 
-from Syllia.apps.syllabus.views import SyllabusView, RubricView
+from Syllia.apps.syllabus import views
 
 urlpatterns = patterns('',
     # url(r'^$', login_required(DashboardView.as_view()), name='index'),
-    url(r'^syllabus/new/$', login_required(SyllabusView.as_view()), name='syllabus'),
-    url(r'^syllabus/(\d+)/$', login_required(SyllabusView.as_view()), name='syllabus'),
-    url(r'^rubric/new/$', login_required(RubricView.as_view()), name='rubric'),
-    url(r'^rubric/(\d+)/$', login_required(RubricView.as_view()), name='rubric'),
-    # url(r'^profile/$', ProfileView.as_view(), name='profile'),
-    # url(r'^register/$', RegisterView.as_view(), name='register'),
+    url(r'^syllabus/new/$', login_required(views.SyllabusView.as_view()), name='syllabus'),
+    url(r'^syllabus/(\d+)/$', login_required(views.SyllabusView.as_view()), name='syllabus'),
+    url(r'^rubric/new/$', login_required(views.RubricView.as_view()), name='rubric'),
+    url(r'^rubric/(\d+)/$', login_required(views.RubricView.as_view()), name='rubric'),
 )
