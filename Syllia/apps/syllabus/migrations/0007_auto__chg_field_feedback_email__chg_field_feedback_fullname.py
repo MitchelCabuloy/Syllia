9cfb069c292d648027b374d352fb29f589862cb4
MitchelCabuloy <mitchel.cabuloy@gmail.com>
2013-11-14 08:59:06 +0800

    Fixed Feedback not working for incomplete fields.

---
+++ Syllia/apps/syllabus/migrations/0007_auto__chg_field_feedback_email__chg_field_feedback_fullname.py
@@ -0,0 +1,102 @@
+# -*- coding: utf-8 -*-
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+
+        # Changing field 'Feedback.email'
+        db.alter_column(u'syllabus_feedback', 'email', self.gf('django.db.models.fields.EmailField')(max_length=75, null=True))
+
+        # Changing field 'Feedback.fullname'
+        db.alter_column(u'syllabus_feedback', 'fullname', self.gf('django.db.models.fields.CharField')(max_length=50, null=True))
+
+    def backwards(self, orm):
+
+        # Changing field 'Feedback.email'
+        db.alter_column(u'syllabus_feedback', 'email', self.gf('django.db.models.fields.EmailField')(default=None, max_length=75))
+
+        # Changing field 'Feedback.fullname'
+        db.alter_column(u'syllabus_feedback', 'fullname', self.gf('django.db.models.fields.CharField')(default=None, max_length=50))
+
+    models = {
+        u'auth.group': {
+            'Meta': {'object_name': 'Group'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '80'}),
+            'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'})
+        },
+        u'auth.permission': {
+            'Meta': {'ordering': "(u'content_type__app_label', u'content_type__model', u'codename')", 'unique_together': "((u'content_type', u'codename'),)", 'object_name': 'Permission'},
+            'codename': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['contenttypes.ContentType']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        },
+        u'authtools.user': {
+            'Meta': {'ordering': "[u'name', u'email']", 'object_name': 'User'},
+            'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'email': ('django.db.models.fields.EmailField', [], {'unique': 'True', 'max_length': '255', 'db_index': 'True'}),
+            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['auth.Group']", 'symmetrical': 'False', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'})
+        },
+        u'contenttypes.contenttype': {
+            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
+            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
+        u'syllabus.college': {
+            'Meta': {'object_name': 'College'},
+            'college_name': ('django.db.models.fields.CharField', [], {'max_length': '50'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'})
+        },
+        u'syllabus.department': {
+            'Meta': {'object_name': 'Department'},
+            'college': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['syllabus.College']"}),
+            'department_name': ('django.db.models.fields.CharField', [], {'max_length': '50'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'})
+        },
+        u'syllabus.feedback': {
+            'Meta': {'object_name': 'Feedback'},
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'null': 'True', 'blank': 'True'}),
+            'feedback': ('django.db.models.fields.TextField', [], {}),
+            'fullname': ('django.db.models.fields.CharField', [], {'max_length': '50', 'null': 'True', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'})
+        },
+        u'syllabus.rubric': {
+            'Meta': {'object_name': 'Rubric'},
+            'created': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'json_data': ('jsonfield.fields.JSONField', [], {'default': "{'pk': 0}"}),
+            'last_modified': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'}),
+            'rubric_name': ('django.db.models.fields.CharField', [], {'max_length': '50'}),
+            'user': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['authtools.User']"})
+        },
+        u'syllabus.syllabus': {
+            'Meta': {'object_name': 'Syllabus'},
+            'course_code': ('django.db.models.fields.CharField', [], {'max_length': '7'}),
+            'created': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'department': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['syllabus.Department']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'json_data': ('jsonfield.fields.JSONField', [], {'default': "{'pk': 0}"}),
+            'last_modified': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'}),
+            'rubric': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['syllabus.Rubric']", 'null': 'True', 'on_delete': 'models.SET_NULL', 'blank': 'True'}),
+            'syllabus_name': ('django.db.models.fields.CharField', [], {'max_length': '50'}),
+            'user': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['authtools.User']"})
+        }
+    }
+
+    complete_apps = ['syllabus']
